dbc364b77cb98f764d9b547645f0cf9397fd2b04
manjeet2k <manjeet.1996.12@gmail.com>
2020-09-16 12:56:21 +0530

    initialised the repo

---
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -0,0 +1,29 @@
+defmodule RumblWeb.UserController do
+  use RumblWeb, :controller
+
+  alias Rumbl.Accounts
+  alias Rumbl.Accounts.User
+
+  def index(conn, _params) do
+    users = Accounts.list_users()
+    render(conn, "index.html", users: users)
+  end
+
+  def show(conn, %{"id" => id}) do
+    user = Accounts.get_user(id)
+    render(conn, "show.html", user: user)
+  end
+
+  def new(conn,_params) do
+    changeset = User.changeset(%User{}) 
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    {:ok, user} = Accounts.create_user(user_params)
+
+    conn
+    |> put_flash(:info, "#{user.name} Created!")
+    |> redirect(to: Routes.user_path(conn, :index))
+  end
+end
